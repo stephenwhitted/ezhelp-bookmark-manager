fc40d0c21897f8d4b29813ce6a196d90306f9e4a
Stephen Whitted <stephent.whitted@gmail.com>
2024-05-03 18:00:07 -0400

    start js script

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('EZ Help Technology Bookmark Manager')
+//console.log('EZ Help Technology Bookmark Manager')
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('bookmarkForm');
+    const urlInput = document.querySelector('#urlInput');
+    const titleInput = document.querySelector('#titleInput');
+    const bookmarkList = document.getElementById('bookmarkList');
+
+  
